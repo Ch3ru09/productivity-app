41d781a937be6ef0d0c6780f1077a3f0784411bf
Ch3ru09 <xliang0905@gmail.com>
2026-03-30 17:33:39 -0400

    added icons to tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,9 @@
 // import { getUser } from "@/services/account";
 import { Tabs, useRouter } from "expo-router";
 import { useEffect } from "react";
+import { Image } from "react-native";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabsLayout() {
   const router = useRouter();
@@ -15,8 +18,24 @@ export default function TabsLayout() {
 
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="calendar" options={{ title: "calendar" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: "calendar",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
